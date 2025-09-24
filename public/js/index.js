6842a8b93a0c1386e280071e1ad547b8721259bc
Jeff Lang <jeff.m.lang@gmail.com>
2025-09-23 18:56:06 -0700

    First commit

---
+++ public/js/index.js
@@ -0,0 +1,13 @@
+
+
+const overlay = document.getElementsByClassName('overlay')[0];
+
+document.addEventListener("keydown", (event) => {
+  if (event.ctrlKey && event.shiftKey && event.key.toLowerCase() === 'u') {
+      event.preventDefault();
+      if (overlay) {
+        console.dir(overlay);
+        overlay.hidden = !overlay.hidden;
+      }
+  }
+});
