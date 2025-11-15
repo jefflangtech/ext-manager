60384666d87be05bb14cfed39ef19922fcf3a6c0
Jeff Lang <jeff.m.lang@gmail.com>
2025-11-14 20:05:20 -0800

    More styling for mode changes; added mode button functionality

--- public/js/index.js
+++ public/js/index.js
@@ -1,28 +1,32 @@
-
+const imgBase = document.documentElement.dataset.imageAssets;
 
 const overlay = document.getElementsByClassName('overlay')[0];
 
 document.addEventListener("keydown", (event) => {
   if (event.ctrlKey && event.shiftKey && event.key.toLowerCase() === 'u') {
-      event.preventDefault();
-      if (overlay) {
-        console.dir(overlay);
-        overlay.hidden = !overlay.hidden;
-      }
+    event.preventDefault();
+    if (overlay) {
+      console.dir(overlay);
+      overlay.hidden = !overlay.hidden;
+    }
   }
 });
 
 const modeToggleBtn = document.querySelector('#mode-switch');
-console.dir(modeToggleBtn);
 
 modeToggleBtn.addEventListener('click', (event) => {
   const rootEl = document.querySelector('html');
   const rootClasses = rootEl.classList;
+  const modeButton = document.querySelector('.theme-toggle__icon');
 
   if(rootClasses.contains('dark')) {
     rootClasses.remove('dark');
+    modeButton.src = `${imgBase}${modeButton.dataset.dark}.svg`;
   } else {
     rootClasses.add('dark');
+    modeButton.src = `${imgBase}${modeButton.dataset.light}.svg`;
   }
+
+  
 
 });
